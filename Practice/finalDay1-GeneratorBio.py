f7dc2a246d65f27d7ff61a9479cae197b009302f
Kahfinity <muhammadalkahfi1900@gmail.com>
2026-03-31 22:38:43 +0700

    New

---
+++ Practice/finalDay1-GeneratorBio.py
@@ -0,0 +1,8 @@
+nama = input("Masukkan nama kamu: ")
+pekerjaan = input("Apa pekerjaan/kesibukanmu?: ")
+kota = input("Di kota mana kamu tinggal?: ")
+
+hasil_bio = "Halo! Saya " + nama + ", seorang " + pekerjaan + " yang berbasis di " + kota + ". Senang berkenalan denganmu!"
+
+print("\n--- Bio Kamu Berhasil Dibuat ---")
+print(hasil_bio)
